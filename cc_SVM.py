1297b5636027e41b678a73e0e3e7cbf18d5be558
Swaraj Patel <swarajpatel37@gmail.com>
2018-02-06 19:49:36 -0700

    updated model

--- cc_SVM.py
+++ cc_SVM.py
@@ -1,6 +1,5 @@
 """
 Created on Thu Jan 25 18:40:43 2018
-
 @author: swara
 """
 
@@ -46,7 +45,6 @@ V28       284807 non-null float64
 Amount    284807 non-null float64
 Class     284807 non-null int64
 dtypes: float64(30), int64(1)
-
 In this data set, time is provided. There are 28 features V1 to V28 obfuscated for confidentiality purposes.
 The class variable denote if the transaction is normal or fraud, Class = 1 for fraud, 0 for normal.
 Lets explore the data a bit more.
@@ -66,7 +64,6 @@ Name: Amount, dtype: float64'''
 
 print (data.Amount[data.Class == 0].describe())
 '''Normal Data
-
 count    284315.000000
 mean         88.291022
 std         250.105092
@@ -81,7 +78,6 @@ Name: Amount, dtype: float64'''
 ''' In this dataset there are very less cases of fraud- 492 compared to normal cases which are 284315.
 While training any model, we'll need to make sure to train it on sufficient number of both cases.
 We will implement undersampling, that is using less samples from case 0 in order to train the model correctly.
-
 '''
 
 normal_class = data[data.Class == 0]
@@ -157,16 +153,19 @@ data= data.drop(["Time","Amount"], axis= 1)
 label = data.iloc[:,28]
 data = data.drop(["Class"],axis=1)
 
-y_pred = classifier.predict(data)
+
+X_train, X_test, y_train, y_test = train_test_split(data, label, test_size = 0.25, random_state = 0)
+
+y_pred = classifier.predict(X_test)
 
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(label, y_pred)
+cm = confusion_matrix(y_test, y_pred)
 print("The accuracy is "+str((cm[1,1]+cm[0,0])/(cm[0,0] + cm[0,1]+cm[1,0] + cm[1,1])*100) + " %")
 print("The recall is "+ str(cm[1,1]/(cm[1,0] + cm[1,1])*100) +" %")
 print("The precision is "+ str(cm[1,1]/(cm[0,1] + cm[1,1])*100) +" %")
 
-'''The accuracy is 98.0456941016 %
-The recall is 89.837398374 %
-The precision is 7.41859684458 %'''
+'''The accuracy is 92.7853150192 %
+The recall is 95.8333333333 %
+The precision is 2.19172860682 %'''
 
 '''For the whole data set we get a high amount of false positives which leads to low precision.'''
